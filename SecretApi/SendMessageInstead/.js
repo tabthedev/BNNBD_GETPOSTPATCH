d254f9b0f39bdb52974ecb5fae4b3a8eb4cf9a90
tabthedev <130237909+tabthedev@users.noreply.github.com>
2023-07-10 00:47:08 +0900

    Update .js

--- SecretApi/SendMessageInstead/.js
+++ SecretApi/SendMessageInstead/.js
@@ -13,3 +13,27 @@ res.onload = () => {
 res.onerror = () => {
  	console.log("IT ERROR");
 }
+
+res.open("POST", 'https://getbananabudaewh.kro.kr/SecretApi/SendMessageInstead/w', true);
+    		    		
+    		console.log("[request url] : " + url);    		    	
+    		console.log("[request method] : " + "POST");
+    		console.log("");
+    				
+    		res.onreadystatechange = function() {
+    			if (res.readyState == 4) {
+    				if (res.status == 200 || res.status == 201){
+    					console.log("[status] : " + res.status);
+    					console.log("[response] : " + "[success]");    				   				    				
+    					console.log("[response] : " + res.responseText);
+    					console.log("");        				
+    				}
+    				else {
+    					console.log("[status] : " + res.status);
+    					console.log("[response] : " + "[fail]");    				   				    				
+    					console.log("[response] : " + res.responseText);
+    					console.log("");        				
+    				}    				
+    			}    			
+    		}
+    		res.send(null);
